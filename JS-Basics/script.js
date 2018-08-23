4480427204c36d39173c514e4a3a7f44506aafce
Chafi Bennassar <bennasserchafi94@gmail.com>
2018-08-23 18:28:53 +0200

    First commit

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -1,1 +1,46 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+
+// Variables
+
+// Declaration
+var name = 'Bennassar'; // single and double quotes are allowed
+console.log("My name is : " + name);
+
+var lastName = 'Chafi';
+console.log("My last name is : " + lastName);
+
+var age = 26;
+console.log("Age is :" + age);
+
+// Data Types
+// in all programming langages there are different data Types for numbers, strings, true-false values ... 
+
+// JavaScript has 5 primitive DATA TYPES: 
+// 1. Number : Floating point numbers, for decimals and integers 
+// 2. String : Sequence characters, used for text
+// 3. Boolean : Logical data type that can only be true or false
+// 4. Undifined : Data type of a variable which does not have a value yet
+// 5. Null : also means 'non-existent' 
+
+// primitive just means that these are not objects, only primitives (simple data types)
+
+// JavaScirpt numbers are always floating point numbers meaning that they always have decimals, even
+// if we sometimes don't see or define them for example 5 just like having 5.0 .	 	 
+
+// Undifined is the data type that is automatically assigned to a variable which does not have value yet.
+
+// JavaScript has dynamic typing which means that we do no have to manually define the data type of a variable
+// JS figure out the data type of a variale on it's own and on the fly	meaning that we can even change 
+// the data type of a variable later in our code. this can be very useful but it can also be the source of some difficulty to find 	 
+// bugs.
+
+var fullAge = true;
+console.log(fullAge);
+
+
+
+
+
+
+
+
